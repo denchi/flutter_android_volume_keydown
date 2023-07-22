8b6c1061c886909285855ecdf700c7b455dbba56
denchi <denis.chiriac@gmail.com>
2023-07-22 11:22:50 +0100

    Switched to media button

--- android/src/main/java/dev/darttools/flutter_android_volume_keydown/FlutterAndroidVolumeKeydownActivity.java
+++ android/src/main/java/dev/darttools/flutter_android_volume_keydown/FlutterAndroidVolumeKeydownActivity.java
@@ -14,6 +14,7 @@ import io.flutter.embedding.android.FlutterActivity;
 public class FlutterAndroidVolumeKeydownActivity extends FlutterActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        /*
         if(keyCode == KEYCODE_VOLUME_DOWN && eventSink != null) {
             eventSink.success(true);
             return true;
@@ -22,6 +23,18 @@ public class FlutterAndroidVolumeKeydownActivity extends FlutterActivity {
             eventSink.success(false);
             return true;
         }
+        */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return super.onKeyDown(keyCode, event);
+        }
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+                eventSink.success(true);
+                return true;
+        }
+
         return super.onKeyDown(keyCode, event);
     }
 }
